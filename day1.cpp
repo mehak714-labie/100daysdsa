e4c7571e04b9423fad499639462e8e6179bf843d
mehak714-labie <mehakaro714@gmail.com>
2026-02-03 22:29:43 +0530

    Add files via upload

--- day1.cpp
+++ day1.cpp
@@ -1,25 +1,23 @@
-//Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
+// Write a C program to delete the element at a given 1-based position pos from an array of n integers. Shift remaining elements to the left.
 #include <stdio.h>
-
 int main() {
-    int a[100], n, i, x, pos;
+    int a[100], n, pos, i;
     printf("Enter number of elements: ");
     scanf("%d", &n);
     printf("Enter elements: ");
     for(i = 0; i < n; i++)
         scanf("%d", &a[i]);
-    printf("Enter element to insert: ");
-    scanf("%d", &x);
-    printf("Enter position (1-based): ");
+    printf("Enter position to delete (1-based): ");
     scanf("%d", &pos);
-    // shift elements to the right
-    for(i = n - 1; i >= pos - 1; i--)
-        a[i + 1] = a[i];
-    a[pos - 1] = x;
-    n++;
-    printf("Array after insertion: ");
+    if(pos < 1 || pos > n) {
+        printf("Invalid position");
+        return 0;}
+    // Shift elements to the left
+    for(i = pos - 1; i < n - 1; i++)
+        a[i] = a[i + 1];
+    n--;
+    printf("Array after deletion: ");
     for(i = 0; i < n; i++)
         printf("%d ", a[i]);
-    return 0;
-}
+    return 0;   }
 
